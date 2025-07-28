98a298cf990b991d6805b5abb41d5184dc23ba26
jykim <jykim@whatap.io>
2025-07-28 18:07:10 +0900

    Remove unused functions and imports related to keep-alive and health-check mechanisms for simplification and cleanup in `main.go`.

--- main.go
+++ main.go
@@ -9,19 +9,14 @@ package main
 
 import (
 	"fmt"
-	whatap_io "github.com/whatap/golib/io"
-	"github.com/whatap/golib/lang/value"
 	"github.com/whatap/golib/logger/logfile"
 	"github.com/whatap/golib/util/dateutil"
 	"log"
-	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"open-agent/open"
 	"open-agent/pkg/config"
-	"open-agent/util/io"
 	"os"
-	"os/exec"
 	"os/signal"
 	"runtime"
 	"runtime/pprof"
@@ -34,23 +29,6 @@ var (
 	version    string // Version of the application
 	commitHash string // Git commit hash
 )
-
-// Constants for the keep-alive mechanism
-const (
-	KEEP_ALIVE_PROTOCOL = 0x5B9B // Protocol identifier for keep-alive messages
-)
-
-// getAliveSockAddr returns the socket address for keep-alive communication
-// It uses the current working directory where main.go is executed
-func getAliveSockAddr() string {
-	// Get the current working directory
-	cwd, err := os.Getwd()
-	if err != nil {
-		// If there's an error getting the current directory, fall back to temp directory
-		return fmt.Sprintf("%s/whatap_openagent.sock", os.TempDir())
-	}
-	return fmt.Sprintf("%s/whatap_openagent.sock", cwd)
-}
 
 // startPprofServer starts the pprof HTTP server for performance profiling
 func startPprofServer(logger *logfile.FileLogger) {
@@ -145,194 +123,6 @@ func exitOnStdinClose(logger *logfile.FileLogger) {
 		time.Sleep(1 * time.Second)
 	}
 }
-func startWorker(command string, logger *logfile.FileLogger) (*exec.Cmd, error) {
-	var cmd *exec.Cmd
-	if config.IsForceStandaloneMode() {
-		logger.Infoln("StartWorker", fmt.Sprintf("Start Worker Process(%s foreground standalone)", command))
-		cmd = exec.Command(command, "foreground", "standalone")
-	} else {
-		logger.Infoln("StartWorker", fmt.Sprintf("Start Worker Process(%s foreground)", command))
-		cmd = exec.Command(command, "foreground")
-	}
-	err := cmd.Start()
-	if err != nil {
-		logger.Infoln("StartWorkerError", err)
-		return nil, err
-	}
-	return cmd, nil
-}
-func superviseRun(childHealthChannel chan bool, logger *logfile.FileLogger) {
-	for {
-		logger.Infoln("superviseRun", "Worker Start")
-		cmd, err := startWorker(os.Args[0], logger)
-		if err != nil {
-			return
-		}
-
-		childAlive := true
-		//childStart := time.Now()
-
-		for childAlive {
-			select {
-			case healthStatus := <-childHealthChannel:
-				if !healthStatus {
-					if cmd != nil && cmd.Process != nil {
-						cmd.Process.Signal(syscall.SIGABRT)
-						err = cmd.Wait()
-						childAlive = false
-					}
-					logger.Infoln("superviseCheck", "child health problem, child kill")
-				}
-			case <-time.After(time.Duration(3) * time.Minute):
-				if cmd != nil && cmd.Process != nil {
-					cmd.Process.Signal(syscall.SIGABRT)
-					err = cmd.Wait()
-					childAlive = false
-				}
-				logger.Infoln("superviseCheck", "child health check timeout, child kill")
-			}
-		}
-
-		if err != nil {
-			logger.Infoln("superviseRunError", "command wait error : ", err)
-		}
-	}
-}
-func sendKeepAliveMessage(logger *logfile.FileLogger, conn net.Conn) {
-	writer := io.NewNetWriteHelper(conn)
-	var err error
-	for err == nil {
-		msgmap := value.NewMapValue()
-		if open.IsOK() {
-			msgmap.PutString("Health", "OK")
-		} else {
-			logger.Infoln("KeepAlive", "HealthCheck Fail")
-			msgmap.PutString("Health", "PROBLEM")
-		}
-
-		dout := whatap_io.NewDataOutputX()
-		dout.WriteShort(KEEP_ALIVE_PROTOCOL)
-		doutx := whatap_io.NewDataOutputX()
-		msgmap.Write(doutx)
-		dout.WriteIntBytes(doutx.ToByteArray())
-		err = writer.WriteBytes(dout.ToByteArray(), 30*time.Second)
-		if err != nil {
-			logger.Infoln("KeepAliveWriteFail", "KeepAliveFail")
-		}
-
-		time.Sleep(60 * time.Second)
-	}
-}
-func keepAliveSender(logger *logfile.FileLogger) {
-	// 루프 밖에서 소켓 주소를 한 번만 계산합니다.
-	sockAddr := getAliveSockAddr()
-	logger.Infoln("keepAliveSender", "Socket address initially set to", sockAddr)
-
-	for {
-		logger.Infoln("keepAliveSender", "keepAliveSender loop starts")
-
-		// 소켓 파일 존재 여부 확인
-		if _, err := os.Stat(sockAddr); os.IsNotExist(err) {
-			logger.Infoln("keepAliveSenderError-1", "keepAliveSocketNotExist, re-evaluating socket address", sockAddr)
-			// 파일이 없으면 주소를 다시 계산하고 잠시 대기 후 루프를 다시 시작합니다.
-			sockAddr = getAliveSockAddr()
-			time.Sleep(10 * time.Second)
-			continue
-		}
-
-		// 연결 시도
-		socktype := "unix"
-		laddr := net.UnixAddr{sockAddr, socktype}
-		conn, err := net.DialUnix(socktype, nil, &laddr)
-
-		// 연결 실패 처리
-		if err != nil {
-			logger.Infoln("keepAliveSenderError-2", "Dial Error", err)
-			time.Sleep(3 * time.Second)
-			continue // 루프의 처음으로 돌아가 다시 시도
-		}
-
-		// 연결 성공 시 메시지 전송
-		sendKeepAliveMessage(logger, conn)
-		conn.Close()
-
-		time.Sleep(10 * time.Second)
-	}
-}
-func keepAlive(conn net.Conn, childHealthChannel chan bool, logger *logfile.FileLogger) {
-	defer func() {
-		if conn != nil {
-			conn.Close()
-		}
-	}()
-
-	reader := io.NewNetReadHelper(conn)
-	for {
-		protocol, err := reader.ReadShort()
-		if err != nil || protocol != KEEP_ALIVE_PROTOCOL {
-			logger.Infoln("keepAliveError-1", err)
-			return
-		}
-
-		msgbytes, err := reader.ReadIntBytesLimit(2048)
-		if err != nil {
-			logger.Infoln("keepAliveError-2", err)
-			return
-		}
-
-		datainputx := whatap_io.NewDataInputX(msgbytes)
-		msgmap := value.NewMapValue()
-		msgmap.Read(datainputx)
-
-		if msgmap.GetString("Health") == "OK" {
-			childHealthChannel <- true
-		} else {
-			childHealthChannel <- false
-		}
-	}
-}
-func monitorChildHealth(childHealthChannel chan bool, logger *logfile.FileLogger) {
-	logger.Infoln("childHealthCheck", "childHealthCheck Start")
-	for {
-		sockAddr := getAliveSockAddr()
-		if err := os.RemoveAll(sockAddr); err != nil {
-			logger.Println("childHealthCheckError-1", err)
-		}
-
-		l, err := net.Listen("unix", sockAddr)
-		if err != nil {
-			logger.Infoln("childHealthCheckError-2", err)
-			continue
-		}
-
-		errorCount := 0
-		for {
-			conn, err := l.Accept()
-			if err != nil {
-				if conn != nil {
-					conn.Close()
-				}
-				logger.Infoln("childHealthCheckError-3", err)
-				errorCount += 1
-				time.Sleep(3 * time.Second)
-			}
-
-			if errorCount > 10 {
-				if conn != nil {
-					conn.Close()
-				}
-
-				logger.Infoln("childHealthCheckError-4", "ErrorCount Over 10")
-				break
-			}
-
-			keepAlive(conn, childHealthChannel, logger)
-		}
-
-		l.Close()
-		time.Sleep(3 * time.Second)
-	}
-}
 
 func main() {
 
@@ -367,31 +157,23 @@ func main() {
 
 			//worker
 			openHome := os.Getenv("WHATAP_OPEN_HOME")
-			logger := logfile.NewFileLogger(logfile.WithOnameLogID("open", "OPEN-AGENT-WORKER"), logfile.WithHomePath(openHome))
+			logger := logfile.NewFileLogger(logfile.WithOnameLogID("open", "OPEN-AGENT"), logfile.WithHomePath(openHome))
 			go exitOnStdinClose(logger)
-			go keepAliveSender(logger)
 			run(openHome, logger)
 		} else if arg1 == "standalone" {
 			if config.IsDebugEnabled() {
-				fmt.Println("Standalone supervisor mode: enabled")
+				fmt.Println("Standalone mode: enabled")
 			}
-			// Set force standalone mode for supervisor
 			config.SetForceStandaloneMode(true)
 		}
 	}
 
-	// master
-	stopper := make(chan os.Signal, 1)
-	signal.Notify(stopper, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	if config.IsDebugEnabled() {
+		fmt.Println("mode:default (worker)")
+	}
 
-	// Create a file to indicate that the agent is running
 	openHome := os.Getenv("WHATAP_OPEN_HOME")
-	logger := logfile.NewFileLogger(logfile.WithOnameLogID("open", "OPEN-AGENT-SUPERVISOR"), logfile.WithHomePath(openHome))
-	childHealthChannel := make(chan bool)
-	go monitorChildHealth(childHealthChannel, logger)
-	time.Sleep(1 * time.Second)
+	logger := logfile.NewFileLogger(logfile.WithOnameLogID("open", "OPEN-AGENT"), logfile.WithHomePath(openHome))
 
-	go superviseRun(childHealthChannel, logger)
-
-	<-stopper
+	run(openHome, logger)
 }
